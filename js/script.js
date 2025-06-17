fd94783ac1ce2fc5d2b2ee2dad1ef55d0dcba145
João Daniel <jdkklcd1234@gmail.com>
2025-06-17 10:15:47 -0300

    vercion-4.2
    atualização da logica do calculo,  e dos valores

--- js/script.js
+++ js/script.js
@@ -21,10 +21,38 @@ function calcularSalario() {
     let outrosDescontos = parseFloat(document.getElementById("outrosDescontos").value) || 0;
 
     // Calcula o desconto do INSS (11,07% do salário bruto)
-    let descontoINSS = salarioBruto * 0.1107;
+    let descontoINSS = 0;
+    if (salarioBruto = 1518.00){
+        descontoINSS = salarioBruto * 0.075;
+    }
+    else if (salarioBruto > 1518.00 && salarioBruto <= 2793.88){
+        descontoINSS = (salarioBruto * 0.09) - 22.77
+    }
+    else if (salarioBruto > 2793.88 && salarioBruto <= 4190.83){
+       descontoINSS = (salarioBruto * 0.12) - 106.59
+    }
+    else {
+        descontoINSS = (salarioBruto * 0.14) - 190.40
+    }
+   
 
     // Calcula o desconto do IRRF (10,66% do salário bruto)
-    let descontoIR = salarioBruto * 0.1066;
+    let descontoIR = 0
+    if (salarioBruto <= 2259.20){
+        descontoIR = 0
+    }
+    else if(salarioBruto > 2259.20 && salarioBruto <= 2826.65){
+        descontoIR = (salarioBruto * 0.075) - 169.44
+    }
+    else if(salarioBruto > 2826.65 && salarioBruto <= 3751.05){
+        descontoIR = (salarioBruto * 0.15) - 381.44
+    }
+    else if(salarioBruto > 3751.05 && salarioBruto <= 4664.68){
+        descontoIR = (salarioBruto * 0.225) - 662.77
+    }
+    else{
+        descontoIR = (salarioBruto * 0.275) - 896.00
+    }
 
     // Calcula o desconto por dependentes (R$ 189,59 por dependente)
     let descontoDependentes = dependentes * 189.59;
